b3b911fd1ba76a75fc8bd6cb63400cbdeafc18fe
HoldenLalumiere <holdenlalumiere@gmail.com>
2024-11-23 14:35:45 -0500

    Added rotate function to vec3d, currently does nothing

--- Unfolding.Client/Polyhedra/DataStructs/Vec3D.cs
+++ Unfolding.Client/Polyhedra/DataStructs/Vec3D.cs
@@ -37,5 +37,23 @@
         {
             return new Vec3D(Y * vec.Z - Z * vec.Y, -(X * vec.Z - Z * vec.X), X * vec.Y - Y * vec.X);
         }
+
+        public void Rotate(double theta, String dimension)
+        {
+            Vec3D[] rotMatrix;
+            if (dimension == "x")
+            {
+                rotMatrix = [new Vec3D(1, 0, 0), new Vec3D(0, Math.Cos(theta), -Math.Sin(theta)), new Vec3D(0, Math.Sin(theta), Math.Cos(theta))];
+            }
+            else if (dimension == "y")
+            {
+                rotMatrix = [new Vec3D(Math.Cos(theta), 0, Math.Sin(theta)), new Vec3D(0, 1, 0), new Vec3D(-Math.Sin(theta), 0, Math.Cos(theta))];
+            }
+            else
+            {
+                rotMatrix = [new Vec3D(Math.Cos(theta), -Math.Sin(theta), 0), new Vec3D(Math.Sin(theta), Math.Cos(theta), 0), new Vec3D(0, 0, 1)];
+            }
+
+        }
     }
 }
