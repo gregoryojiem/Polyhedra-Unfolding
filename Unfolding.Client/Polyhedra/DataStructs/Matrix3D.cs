20b8d4c5713589d3c5f32e7ecdd83f28865a06aa
HoldenLalumiere <holdenlalumiere@gmail.com>
2024-11-23 16:32:59 -0500

    Updated rotation function to be faster by using less multiplication and saving more variables

--- Unfolding.Client/Polyhedra/DataStructs/Matrix3D.cs
+++ Unfolding.Client/Polyhedra/DataStructs/Matrix3D.cs
@@ -25,7 +25,7 @@
 
         public static Matrix3D GetIdentityMatrix()
         {
-            return new Matrix3D(new double[] { 1, 0, 0, 0, 1, 0, 0, 0, 1 });
+            return new Matrix3D([1, 0, 0, 0, 1, 0, 0, 0, 1]);
         }
 
         public static Matrix3D operator +(Matrix3D m1, Matrix3D m2)
@@ -40,7 +40,7 @@
 
         public static Matrix3D operator *(Matrix3D m1, Matrix3D m2)
         {
-            Matrix3D result = new Matrix3D();
+            Matrix3D result = new();
             for (int i = 0; i < 3; i++)
             {
                 for (int j = 0; j < 3; j++)
@@ -64,19 +64,28 @@
             double y = rotationAxis.Y;
             double z = rotationAxis.Z;
 
+            double oneMinusCosThetaX = oneMinusCosTheta * x;
+            double oneMinusCosThetaZ = oneMinusCosTheta * z;
+            double oneMinusCosThetaXY = oneMinusCosThetaX * y;
+            double oneMinusCosThetaXZ = oneMinusCosThetaX * z;
+            double oneMinusCosThetaZY = oneMinusCosThetaZ * y;
+            double sinThetaX = sinTheta * x;
+            double sinThetaY = sinTheta * y;
+            double sinThetaZ = sinTheta * z;
+
             var rotationMatrix = new Matrix3D();
 
-            rotationMatrix[0, 0] = cosTheta + oneMinusCosTheta * x * x;
-            rotationMatrix[0, 1] = oneMinusCosTheta * x * y - sinTheta * z;
-            rotationMatrix[0, 2] = oneMinusCosTheta * x * z + sinTheta * y;
+            rotationMatrix[0, 0] = cosTheta + oneMinusCosThetaX * x;
+            rotationMatrix[0, 1] = oneMinusCosThetaXY - sinThetaZ;
+            rotationMatrix[0, 2] = oneMinusCosThetaXZ + sinThetaY;
 
-            rotationMatrix[1, 0] = oneMinusCosTheta * y * x + sinTheta * z;
+            rotationMatrix[1, 0] = oneMinusCosThetaXY + sinThetaZ;
             rotationMatrix[1, 1] = cosTheta + oneMinusCosTheta * y * y;
-            rotationMatrix[1, 2] = oneMinusCosTheta * y * z - sinTheta * x;
+            rotationMatrix[1, 2] = oneMinusCosThetaZY - sinThetaX;
 
-            rotationMatrix[2, 0] = oneMinusCosTheta * z * x - sinTheta * y;
-            rotationMatrix[2, 1] = oneMinusCosTheta * z * y + sinTheta * x;
-            rotationMatrix[2, 2] = cosTheta + oneMinusCosTheta * z * z;
+            rotationMatrix[2, 0] = oneMinusCosThetaXZ - sinThetaY;
+            rotationMatrix[2, 1] = oneMinusCosThetaZY + sinThetaX;
+            rotationMatrix[2, 2] = cosTheta + oneMinusCosThetaZ * z;
 
 
             return rotationMatrix;
