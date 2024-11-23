576c834b3330dfe75277d994572126c68469d1fe
HoldenLalumiere <holdenlalumiere@gmail.com>
2024-11-23 15:55:44 -0500

    Started work on the net

---
+++ Unfolding.Client/Polyhedra/DataStructs/Net2D.cs
@@ -0,0 +1,29 @@
+﻿namespace Unfolding.Client.Polyhedra.DataStructs
+{
+    public class Net2D
+    {
+        public Net2D()
+        {
+
+        }
+
+        //private static bool LineIntersection(Edge l1, Edge l2)
+        //{
+        //    try
+        //    {
+        //        //l2.slope = (ps[1].y - ps[0].y) / (ps[1].x - ps[0].x)
+        //        XIntersection = (l2.Z - l2.slope * l2.X - l1.Z + l1.slope * l1.X) / (l1.slope - l2.slope);
+        //    }
+        //    catch (DivideByZeroException e)
+        //    {
+
+        //    }
+
+        //    if (x <= intersect <= Math.Min(l1.points[1].x, l2.points[1].x))
+        //    {
+        //        return true;
+        //    }
+        //    return false;
+        //}
+    }
+}
