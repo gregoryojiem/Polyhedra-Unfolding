07c5d7355ea24ac3542275860af0ac5e155602dd
Gregory Ojiem <gro3228@rit.edu>
2024-11-24 17:40:20 -0500

    Added an edge class

---
+++ Unfolding.Client/Polyhedra/DataStructs/2D/Edge.cs
@@ -0,0 +1,24 @@
+﻿namespace Unfolding.Client.Polyhedra.DataStructs
+{
+    public class Edge
+    {
+        public Point2D Start { get; set; }
+        public Point2D End { get; set; }
+
+        public Point2D Mid
+        {
+            get
+            {
+                double X = (Start.X + End.X) / 2;
+                double Y = (Start.Y + End.Y) / 2;
+                return new(X, Y);
+            }
+        }
+
+        public Edge(Point2D start, Point2D end)
+        {
+            Start = start;
+            End = end;
+        }
+    }
+}
