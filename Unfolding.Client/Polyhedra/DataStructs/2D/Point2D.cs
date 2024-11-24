6237946d0350eef36fa3b75a52890cdc2b1f709d
HoldenLalumiere <holdenlalumiere@gmail.com>
2024-11-24 17:33:54 -0500

    Giving point and polygon required functions

--- Unfolding.Client/Polyhedra/DataStructs/2D/Point2D.cs
+++ Unfolding.Client/Polyhedra/DataStructs/2D/Point2D.cs
@@ -1,8 +1,18 @@
-﻿namespace Unfolding.Client.Polyhedra.DataStructs
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Unfolding.Client.Polyhedra.DataStructs
 {
     public class Point2D
     {
         public double X { get; set; }
         public double Y { get; set; }
+
+        public Point2D(double X, double Y) { this.X = X; this.Y = Y; }
+
+        public void Rotate(double theta)
+        {
+            X = X * Math.Cos(theta) - Y * Math.Sin(theta);
+            Y = X * Math.Sin(theta) + Y * Math.Cos(theta);
+        }
     }
 }
--- Unfolding.Client/Polyhedra/DataStructs/2D/Polygon.cs
+++ Unfolding.Client/Polyhedra/DataStructs/2D/Polygon.cs
@@ -3,5 +3,26 @@
     public class Polygon
     {
         public Point2D[] Vertices { get; set; }
+        public Point2D Centroid { get
+            {
+                double X = Vertices.Sum(p => p.X) / Vertices.Length;
+                double Y = Vertices.Sum(p => p.Y) / Vertices.Length;
+                return new(X, Y);
+            }
+        }
+
+        public void Rotate(double theta)
+        {
+            for (int i = 0; i < Vertices.Length; i++)
+            {
+                Vertices[i].Rotate(theta);
+            }
+        }
+
+        public double FindConnectionAngle(Polygon poly)
+        {
+
+            return null;//Math.Acos(a.Dot(b) / );
+        }
     }
 }
