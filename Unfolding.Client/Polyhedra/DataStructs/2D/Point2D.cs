8f8fa2b82156b9d2e92ab157ab085f6b5291c41b
Gregory Ojiem <gro3228@rit.edu>
2024-11-24 18:23:18 -0500

    Converting polyhedra to 2D pieces finished

--- Unfolding.Client/Polyhedra/DataStructs/2D/Point2D.cs
+++ Unfolding.Client/Polyhedra/DataStructs/2D/Point2D.cs
@@ -14,5 +14,32 @@ namespace Unfolding.Client.Polyhedra.DataStructs
             X = X * Math.Cos(theta) - Y * Math.Sin(theta);
             Y = X * Math.Sin(theta) + Y * Math.Cos(theta);
         }
+
+        public static Point2D[] SortPoints(Point2D[] points)
+        {
+            double avgX = points.Average(p => p.X);
+            double avgY = points.Average(p => p.Y);
+
+            Point2D centerPoint = new Point2D(avgX, avgY);
+
+            var anglesAndIndices = points.Select((point, index) =>
+            {
+                double dx = point.X - centerPoint.X;
+                double dy = point.Y - centerPoint.Y;
+                double angle = Math.Atan2(dy, dx); 
+                return (angle, index, point);
+            }).ToList();
+
+            anglesAndIndices.Sort((a, b) => a.angle.CompareTo(b.angle));
+
+            return anglesAndIndices.Select(item => item.point).ToArray();
+
+
+        }
+
+        public override string ToString()
+        {
+            return "(" + X + ", " + Y + ")";
+        }
     }
 }
--- Unfolding.Client/Polyhedra/DataStructs/2D/Polygon.cs
+++ Unfolding.Client/Polyhedra/DataStructs/2D/Polygon.cs
@@ -16,6 +16,45 @@ namespace Unfolding.Client.Polyhedra.DataStructs
             }
         }
 
+        public Polygon(Point2D[] vertices, Edge[] edges)
+        {
+            Vertices = vertices;
+            Edges = edges;
+        }
+
+        public static Polygon[] PolyhedraToPolygons(Polyhedron polyhedron)
+        {
+            Polygon[] polygons = new Polygon[polyhedron.Faces.Length];
+
+            for (int i = 0; i < polyhedron.Faces.Length; i++)
+            {
+                PolyhedraFace face = polyhedron.Faces[i];
+                PolyhedraFace alignedFace = face.Rotate3DToAlign();
+                Point2D[] vertices2D = new Point2D[alignedFace.Vertices.Length];
+
+                for (int j = 0; j < alignedFace.Vertices.Length; j++)
+                {
+                    vertices2D[j] = new Point2D(alignedFace.Vertices[j].X, alignedFace.Vertices[j].Z);
+                }
+
+                var sortedVertices = Point2D.SortPoints(vertices2D);
+                int numVertices = sortedVertices.Length;
+                var edges = new Edge[numVertices];
+                
+                for (int j = 0; j < numVertices; j++)
+                {
+                    Point2D start = sortedVertices[j];
+                    Point2D end = sortedVertices[(j + 1) % numVertices];
+                    edges[j] = new Edge(start, end);
+                }
+
+                polygons[i] = new Polygon(sortedVertices, edges);
+
+            }
+
+            return polygons;
+        }
+
         public void Rotate(double theta)
         {
             for (int i = 0; i < Vertices.Length; i++)
