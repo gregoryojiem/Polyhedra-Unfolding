f5fa4fdb44be831b8719cf418205d6607ad656d0
HoldenLalumiere <holdenlalumiere@gmail.com>
2024-11-24 18:34:03 -0500

    Moved line intersection to edge class

--- Unfolding.Client/Polyhedra/DataStructs/2D/Edge.cs
+++ Unfolding.Client/Polyhedra/DataStructs/2D/Edge.cs
@@ -30,5 +30,25 @@
             Vec2D otherVec = new(otherEdge.End.X - otherEdge.Start.X, otherEdge.End.Y - otherEdge.Start.Y);
             return Math.Acos(vec.Dot(otherVec) / (vec.Magnitude * otherVec.Magnitude));
         }
+
+        private static bool Intersection(Edge l)
+        {
+            //    try
+            //    {
+            //        //l2.slope = (ps[1].y - ps[0].y) / (ps[1].x - ps[0].x)
+            //        XIntersection = (l2.Z - l2.slope * l2.X - l1.Z + l1.slope * l1.X) / (l1.slope - l2.slope);
+            //    }
+            //    catch (DivideByZeroException e)
+            //    {
+
+            //    }
+
+            //    if (x <= intersect <= Math.Min(l1.points[1].x, l2.points[1].x))
+            //    {
+            //        return true;
+            //    }
+            //    return false;
+            return false;
+        }
     }
 }
--- Unfolding.Client/Polyhedra/DataStructs/2D/Net2D.cs
+++ Unfolding.Client/Polyhedra/DataStructs/2D/Net2D.cs
@@ -2,23 +2,25 @@
 {
     public class Net2D
     {
-        PolyhedraFace[] faces;
-        public Net2D(PolyhedraFace[] faces)
+        Polygon[] polygons;
+        bool[] setPolygons;
+        public Net2D(Polygon[] polys)
         {
-            this.faces = faces;
+            polygons = polys;
+            setPolygons = new bool[polygons.Length];
         }
 
-        private static Net2D GenerateNet(PolyhedraFace[] faces)
+        private static Net2D GenerateNet(Polygon[] polys)
         {
-            PolyhedraFace largestPoly = faces[0];
-            for (int i = 1; i < faces.Length; i++)
+            Polygon largestPoly = polys[0];
+            for (int i = 1; i < polys.Length; i++)
             {
-                if (faces[i].Vertices.Length > largestPoly.Vertices.Length)
+                if (polys[i].Vertices.Length > largestPoly.Vertices.Length)
                 {
-                    largestPoly = faces[i];
+                    largestPoly = polys[i];
                 }
             }
-            Net2D net = new(faces);
+            Net2D net = new(polys);
             return GenerateNetBacktrack(net);
         }
 
@@ -26,25 +28,5 @@
         {
             return null;
         }
-
-
-        //private static bool LineIntersection(Edge l1, Edge l2)
-        //{
-        //    try
-        //    {
-        //        //l2.slope = (ps[1].y - ps[0].y) / (ps[1].x - ps[0].x)
-        //        XIntersection = (l2.Z - l2.slope * l2.X - l1.Z + l1.slope * l1.X) / (l1.slope - l2.slope);
-        //    }
-        //    catch (DivideByZeroException e)
-        //    {
-
-        //    }
-
-        //    if (x <= intersect <= Math.Min(l1.points[1].x, l2.points[1].x))
-        //    {
-        //        return true;
-        //    }
-        //    return false;
-        //}
     }
 }
