063e38d49d988a03525f61f11c0dfb9e5ef663b3
HoldenLalumiere <holdenlalumiere@gmail.com>
2024-11-26 18:12:39 -0500

    Added solver classes

---
+++ Unfolding.Client/Polyhedra/Solvers/DFS.cs
@@ -0,0 +1,13 @@
+﻿using Unfolding.Client.Polyhedra.DataStructs;
+
+namespace Unfolding.Client.Polyhedra.Solvers
+{
+    public class DFS : Solver
+    {
+        public override Net2D Solve(Net2D unplaced)
+        {
+
+            return null;
+        }
+    }
+}
