aa2fe691891c4293056b31169073131be77dbbe8
HoldenLalumiere <holdenlalumiere@gmail.com>
2024-11-26 19:22:45 -0500

    Half implemented BFS
    Needs to be thought over more.

--- Unfolding.Client/Polyhedra/Solvers/BFS.cs
+++ Unfolding.Client/Polyhedra/Solvers/BFS.cs
@@ -4,9 +4,23 @@ namespace Unfolding.Client.Polyhedra.Solvers
 {
     public class BFS : Solver
     {
-        public override Net2D Solve(Net2D unplaced)
+        public override Net2D Solve(Net2D net)
         {
-
+            // TODO needs proper thought to work properly
+            List<(Polygon, Polygon)> moves = net.GetMoves();
+            foreach (var move in moves)
+            {
+                net.PlacePolygon(move.Item1, move.Item2);
+                if (net.GetStatus() == NetStatus.Invalid)
+                {
+                    net.Undo();
+                }
+            }
+            Solve(net); // Solve on every valid move
+            if (net.GetStatus() == NetStatus.Complete)
+            {
+                return net;
+            }
             return null;
         }
     }
